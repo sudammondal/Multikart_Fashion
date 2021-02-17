08ba1e80dec48abef464e0b79ed786a42b0f5a93
Sudam Mondal <sudammondal02@gmail.com>
2021-02-17 16:44:18 +0600

    add responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,35 +21,35 @@ $(document).ready(function() {
         speed: 3000,
         slidesToShow: 4,
         slidesToScroll: 1,
-        // responsive: [{
-        //         breakpoint: 1024,
-        //         settings: {
-        //             slidesToShow: 3,
-        //             dots: false,
-        //         }
-        //     },
-        //     {
-        //         breakpoint: 767,
-        //         settings: {
-        //             slidesToShow: 2,
-        //             dots: false,
-        //         }
-        //     },
-        //     {
-        //         breakpoint: 600,
-        //         settings: {
-        //             slidesToShow: 2,
-        //             dots: false,
-        //         }
-        //     },
-        //     {
-        //         breakpoint: 480,
-        //         settings: {
-        //             slidesToShow: 1,
-        //             dots: false,
-        //         }
-        //     }
-        // ]
+        responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    dots: false,
+                }
+            },
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 2,
+                    dots: false,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    dots: false,
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    dots: false,
+                }
+            }
+        ]
     });
     $('.bolog_slidmain').slick({
         arrows: true,
@@ -60,35 +60,35 @@ $(document).ready(function() {
         speed: 3000,
         slidesToShow: 3,
         slidesToScroll: 1,
-        // responsive: [{
-        //         breakpoint: 1024,
-        //         settings: {
-        //             slidesToShow: 3,
-        //             dots: false,
-        //         }
-        //     },
-        //     {
-        //         breakpoint: 767,
-        //         settings: {
-        //             slidesToShow: 2,
-        //             dots: false,
-        //         }
-        //     },
-        //     {
-        //         breakpoint: 600,
-        //         settings: {
-        //             slidesToShow: 2,
-        //             dots: false,
-        //         }
-        //     },
-        //     {
-        //         breakpoint: 480,
-        //         settings: {
-        //             slidesToShow: 1,
-        //             dots: false,
-        //         }
-        //     }
-        // ]
+        responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    dots: false,
+                }
+            },
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 1,
+                    dots: false,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    dots: false,
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    dots: false,
+                }
+            }
+        ]
     });
     $('.brand_slider').slick({
         arrows: true,
@@ -99,35 +99,35 @@ $(document).ready(function() {
         speed: 3000,
         slidesToShow: 5,
         slidesToScroll: 1,
-        // responsive: [{
-        //         breakpoint: 1024,
-        //         settings: {
-        //             slidesToShow: 3,
-        //             dots: false,
-        //         }
-        //     },
-        //     {
-        //         breakpoint: 767,
-        //         settings: {
-        //             slidesToShow: 2,
-        //             dots: false,
-        //         }
-        //     },
-        //     {
-        //         breakpoint: 600,
-        //         settings: {
-        //             slidesToShow: 2,
-        //             dots: false,
-        //         }
-        //     },
-        //     {
-        //         breakpoint: 480,
-        //         settings: {
-        //             slidesToShow: 1,
-        //             dots: false,
-        //         }
-        //     }
-        // ]
+        responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 4,
+                    dots: false,
+                }
+            },
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 3,
+                    dots: false,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    dots: false,
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 2,
+                    dots: false,
+                }
+            }
+        ]
     });
     function isoTopActive() {
 		// Iso top js
